1da5158208e2f550b77cd5430d51ad13caf35bf0
Jennifer Chen <matt@thrivesmart.com>
2009-02-19 10:32:56 -1000

    script install fixes

--- install.rb
+++ install.rb
@@ -1,3 +1,3 @@
 # Install hook code here
-require File.join(File.dirname(__FILE__), 'lib', 'tsapp')
+require File.join(File.dirname(__FILE__), 'lib', 'tsapp_rails')
 install File.join( 'config', 'tsapp.yml.sample' )
